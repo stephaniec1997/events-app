7d95bb73137a48915ab3703d2c809d005b655c90
Stephanie Castaneda <stephaniec1997@yahoo.com>
2019-09-12 16:06:24 -0700

    changed variable

--- server.js
+++ server.js
@@ -14,12 +14,11 @@ const port = process.env.PORT || 5000;
 
 
 
-app.use(express.static(path.join(__dirname, "client", "build")))
 
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = process.env.HEROKU_URI || process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
 );
 const connection = mongoose.connection;
@@ -38,6 +37,10 @@ const resendRouter = require('./routes/resend');
 
 app.use('/confirmation', confirmationRouter);
 app.use('/resend', resendRouter);
+
+
+app.use(express.static(path.join(__dirname, "client", "build")))
+
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
