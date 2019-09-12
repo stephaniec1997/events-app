5eab2cc95f6e1b7fedff29341a58dfe6e04e29c9
Stephanie Castaneda <stephaniec1997@yahoo.com>
2019-09-12 15:35:27 -0700

    Reorganized directory in order to deploy on heroku

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const path = require("path")
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+const eventsRouter = require('./routes/events');
+const usersRouter = require('./routes/users');
+
+app.use('/events', eventsRouter);
+app.use('/users', usersRouter);
+
+const confirmationRouter = require('./routes/confirmation');
+const resendRouter = require('./routes/resend');
+
+app.use('/confirmation', confirmationRouter);
+app.use('/resend', resendRouter);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
